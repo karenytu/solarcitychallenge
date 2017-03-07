6de5a9cab0578872b3b98460ee2dbab777d90e55
Karen Tu <karen.2.tu@gmail.com>
2017-03-06 20:17:13 -0800

    try submitting 2

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 	$uri = "mongodb://<dbuser>:<dbpassword>@ds121190.mlab.com:21190/heroku_5kdpcthd";
 	$client = new MongoDB\Client($uri);
 
-	if(isset($_POST['submit'])){ //check if form was submitted
+	if(!empty($_POST)){ //check if form was submitted
 		// $input = $_POST['inputText']; //get input text
 		echo "Success! You entered: ".$_POST['name'];//.$input;
 	}
