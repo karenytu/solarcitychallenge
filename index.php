4e62acd1e4cd7fe34d5b1658d5943457343a9c8a
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 02:11:25 -0800

    inserted missing html tags

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 	<body>
 
 	<form action = "process.php", method="post"> <!-- Write css layout for a form!!!-- >
-		<label>Username <label><input type="text" name="username">
+		<label>Username </label><input type="text" name="username"/>
 		<br>
-		<label>Password <label><input type="text" name="password">
+		<label>Password </label><input type="text" name="password"/>
 		<br>
-		<label>Name <label><input type="text" name="name">
-		<br>
-		<br>
-		<label>Address Line 1<label><input type="text" name="address1"> <!-- Street address, P.O. box, company name, c/o,   <div class="submissionfield">-->
-		<br>
-		<label>Address Line 2<label><input type="text" name="address2"> <!-- Apartment, suite, unit, building, floor, etc.,     <div class="submissionbutton"> -->
-		<br>
-		<label>City <label><input type="text" name="city">
-		<br>
-		<label>State/Province/Region <label><input type="text" name="region">	
-		<br>
-		<label>Country <label><input type="text" name="country">
+		<label>Name </label><input type="text" name="name"/>
 		<br>
 		<br>
-		<label>Why you are interested in solar: <label><input type="text" name="interest">
+		<label>Address Line 1 </label><input type="text" name="address1"/> <!-- Street address, P.O. box, company name, c/o,   <div class="submissionfield">-->
+		<br>
+		<label>Address Line 2 </label><input type="text" name="address2"/> <!-- Apartment, suite, unit, building, floor, etc.,     <div class="submissionbutton"> -->
+		<br>
+		<label>City <label/><input type="text" name="city"/>
+		<br>
+		<label>State/Province/Region <label/><input type="text" name="region"/>	
+		<br>
+		<label>Country <label/><input type="text" name="country"/>
+		<br>
+		<br>
+		<label>Why you are interested in solar: <label/><input type="text" name="interest">
 			<!-- .submissionfield { width: 90px; height: 390px; border: 1px solid #999999; padding: 5px; } for this one-->
 		<br>
 		<input type="submit" name="submit">
