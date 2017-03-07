a12d42df0b27a17212cff94ba1d63d9bb91295bb
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 00:24:08 -0800

    checking table

--- process.php
+++ process.php
@@ -19,7 +19,7 @@
 		$tablecreated = pg_query($resource, "CREATE TABLE users(NAME TEXT);");
 	} else{
 		echo 'table exists!!!!!';
-		$numrows = pg_quert($resource, "SELECT COUNT(*) FROM users;");
+		$numrows = pg_query($resource, "SELECT COUNT(*) FROM users;");
 		echo $numrows;
 		//insert a row..
 		// number of rows in table???
