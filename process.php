8286306232b107624be65b1068e9a4661f8a5999
Karen Tu <karen.2.tu@gmail.com>
2017-03-06 23:27:11 -0800

    passing name

--- process.php
+++ process.php
@@ -7,6 +7,7 @@
 
  <?php
 	$resource =  pg_connect(getenv('DATABASE_URL'));
+	echo 'test';
 	echo pg_name(resource);
 	$numrows = pg_num_rows(resource, "select * from users"); //users is the name of a table
 	// $required = array('name'); implement later...https://stackoverflow.com/questions/3190464/php-check-if-any-posted-vars-are-empty-form-all-fields-required
