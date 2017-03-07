db7aaaef8d1a79126c1e1d50509c9460d9e0eee9
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 15:53:21 -0800

    added missing quote

--- database.php
+++ database.php
@@ -7,7 +7,7 @@
 			echo "<table>"; // start table in html, determine style in a css file!!!!
 			echo "<tr><td>username</td><td>password</td><td>name</td><td>age</td><td>address</td><td>interest</td></tr>";
 			while($row = pg_fetch_array($table)) {
-				echo str_replace("\\n", "<br>",<tr><td>".$row['username']."</td><td>".$row['password']."</td><td>".$row['name']."</td><td>".$row['age']."</td><td>".$row['address']."</td><td>".$row['interest']."</td></tr>");
+				echo str_replace("\\n", "<br>", "<tr><td>".$row['username']."</td><td>".$row['password']."</td><td>".$row['name']."</td><td>".$row['age']."</td><td>".$row['address']."</td><td>".$row['interest']."</td></tr>");
 			}
 			echo "</table>"; // needed to close the table in html
 			pg_close();
