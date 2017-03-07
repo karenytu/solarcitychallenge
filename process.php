e8eb348a6ac1c9d6963a91c8b74da429bf4e99db
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 15:01:28 -0800

    bug possibly caught 3

--- process.php
+++ process.php
@@ -14,10 +14,10 @@
 		!empty($_POST["city"]) && !empty($_POST["region"]) && !empty($_POST["country"]) &&
 		!empty($_POST["interest"])) {
 
-		if (!pg_query("if exists (select name from users where username = \'".$POST["username"]."\') then null")) { //username already exists
+		if (pg_query("if exists (select name from users where username = \'".$POST["username"]."\') then null")) { //username already exists
 			//"select name from users where username = \'".$POST["username"]."\'"
 			//"if exists (select name from users where username = \'".$POST["username"]."\') then null"
-			var_dump(pg_query("if exists (select name from users where username = \'".$POST["username"]."\') then null"));
+			//var_dump(pg_query("if exists (select name from users where username = \'".$POST["username"]."\') then null"));
 			echo 'username already exists, go back and choose a different one'; // make this into an html pop up or something
 			echo "<html>
 				<body>
