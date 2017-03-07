e0caa9fcb6ac5ad2bd062f1871ae80d344b01c4e
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 15:09:07 -0800

    escape quotes

--- process.php
+++ process.php
@@ -42,7 +42,7 @@
 			//echo $serializedaddress;
 
 			//put all information into a string, then send to database
-			$rowtoinsert = "\'".$_POST["username"]."\','".$_POST["password"]."\','".$_POST["name"]."\','".$address."\','"$_POST['interest']."\'";
+			$rowtoinsert = "\'".$_POST["username"]."\',\'".$_POST["password"]."\',\'".$_POST["name"]."\',\'".$address."\',\'"$_POST['interest']."\'";
 			echo $rowtoinsert;
 			//pg_query($resource, "insert into users values (".$rowtoinsert. ");");
 		}
