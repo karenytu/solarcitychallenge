0f6be2c1b42bd37d382e93ae6c5170f74550dd32
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 01:13:45 -0800

    try inserting a row

--- process.php
+++ process.php
@@ -6,26 +6,15 @@
 </html>
 
  <?php
-	$resource =  pg_connect(getenv('DATABASE_URL'));
-	echo 'test';
-	$name = pg_dbname(); //definitely returned some random string!
-	echo 'test 2';
+	$resource =  pg_connect(getenv('DATABASE_URL')); // postgres://zbvkfthmvsspzz:c57aa9f724109cc6ad419575424b765a10ee05205091f8048ad24c607545b3c8@ec2-23-21-186-138.compute-1.amazonaws.com:5432/d5arjtlq5lmgqh
 	$result = pg_query($resource, "IF OBJECT_ID('users') IS NULL");
-	$numtables = pg_query($resource, "select count(*) from information_schema.tables");
-	echo $numtables ? 'true' : 'false';
-	// echo 4;
-	if ($result) {
-		echo 'table does not exist';
-		$tablecreated = pg_query($resource, "CREATE TABLE users(NAME TEXT);");
-	} else{
-		echo 'table exists!!!!!';
-		$table = pg_query($resource, "SELECT * from users;");
-		$row = pg_fetch_row($resource, 0);
-		var_dump(pg_num_rows($table));
-		// $numrows = pg_query($resource, "SELECT COUNT(*) FROM users;");
-		// echo $numrows;
-		//insert a row..
-		// number of rows in table???
+
+
+	echo 'table exists!!!!!';
+	//insert a row..
+	$insertrow = pg_query($resource, "insert into users values('un2', 'pw2', 'name2', '2', 'address2', 'interest2');");
+	if($insertrow) {
+		echo 'information entered successfully!';
 	}
 
 	// $table = pg_query($resource, "SELECT * from users;");
@@ -37,29 +26,4 @@
  //   				// <td>", $row["id"], "</td></tr>";
  //   }
 
-	$result2 = pg_query($resource, "IF OBJECT_ID('users') IS NULL");
-	if ($result2) {
-		echo 'table NOT created';
-	}
-	echo 'test 3';
-	// $result2 = pg_query($resource, "IF OBJECT_ID('users') IS NULL");
-	// if (!$result2) {
-	// 	echo 'table does not exist';
-	// }
-	// echo 'test 4';
-	// if(pg_query($resource, 'IF OBJECT_ID(\'users\') IS NULL') {
-	// 	pg_query($resource, 'CREATE TABLE users(NAME TEXT)');
-	// 	echo 'table created';
-	// } else {
-	// 	echo 'table already exists';
-	// }
-	// pg_copy_from($resource, 'users', $rows);
-	// $numrows = pg_num_rows(resource, "select * from users"); //users is the name of a table
-	// $required = array('name'); implement later...https://stackoverflow.com/questions/3190464/php-check-if-any-posted-vars-are-empty-form-all-fields-required
-	// echo $_POST["name"];
-	// if(empty($POST)) {
-	// 	echo 'empty';
-	// } else {
-	// 	echo 'filled';
-	// }
 ?>
