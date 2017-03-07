889f6cec73bc35b52783d6927bd123c377c0b192
Karen Tu <karen.2.tu@gmail.com>
2017-03-06 21:51:46 -0800

    trying postgresql

--- index.php
+++ index.php
@@ -28,27 +28,7 @@
 
 <?php  
 	echo 'Test 1';
-	$dbopts = parse_url(getenv('DATABASE_URL'));
-		$app->register(new Herrera\Pdo\PdoServiceProvider(),
-		               array(
-		                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-		                   'pdo.username' => $dbopts["user"],
-		                   'pdo.password' => $dbopts["pass"]
-		               )
-		);
+	$resource =  pg_connect(getenv('DATABASE_URL'));
+	echo 'Test 2';
 	
-	$app->get('/db/', function() use($app) {
-	$st = $app['pdo']->prepare('SELECT name FROM test_table');
-	$st->execute();
-
-	$names = array();
-	while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-	   	$app['monolog']->addDebug('Row ' . $row['name']);
-	    $names[] = $row;
-	}
-
-	return $app['twig']->render('database.twig', array(
-	    'names' => $names
-	  ));
-	});
  ?>
