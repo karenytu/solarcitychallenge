78c3d23d9b3829bb0e5bfa85c4f7b5d205412c64
Karen Tu <karen.2.tu@gmail.com>
2017-03-06 20:39:18 -0800

    try if

--- index.php
+++ index.php
@@ -31,8 +31,13 @@
 	$uri = "mongodb://heroku_5kdpcthd:rggk6pekbv8ttf1f7g6ju7jp5u@ds121190.mlab.com:21190/heroku_5kdpcthd";
 	// $uri = getenv('MONGODB_URI');
 	echo 'Hello World 1';
-	echo 'Hello World 3';
+	if(!empty($_POST)){ //check if form was submitted
+		// $input = $_POST['inputText']; //get input text
+		echo 'Hello World 2';
+		echo "Success! You entered: ".$_POST['name'];//.$input;
+	}
 	$client = new MongoDB\Client("mongodb://heroku_5kdpcthd:rggk6pekbv8ttf1f7g6ju7jp5u@ds121190.mlab.com:21190/heroku_5kdpcthd");
+	echo 'Hello World 3';
 
 	echo 'Hello World';
 	if(!empty($_POST)){ //check if form was submitted
