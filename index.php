4b4757bd21bd200636d22cc732f30123ef7a4a4e
Karen Tu <karen.2.tu@gmail.com>
2017-03-07 02:21:14 -0800

    try printing out table, insert another row

--- index.php
+++ index.php
@@ -3,38 +3,17 @@
 	<body>
 
 	<form action = "process.php" method="post"> <!-- Write css layout for a form!!!-- >
-		Username <input type="text" name="username"/>
-		<div>
-			<label>Password </label>
-			<input type="text" name="password">
-		</div>
-		<div>
-			<label>Password </label>
-			<input type="text" name="password">
-		</div>
-		<div>
-			<label>Password </label>
-			<input type="text" name="password">
-		</div>
-		<div>
-			<label>Password </label>
-			<input type="text" name="password">
-		</div>
-		<div>
-			<label>Password </label>
-			<input type="text" name="password">
-		</div>
+		<label>Address Line 2 </label><input type="text" name="address2"/> <!-- Apartment, suite, unit, building, floor, etc.,     <div class="submissionbutton"> -->
+		<br>
+		<label>Username </label<input type="text" name="username"/>
+		<br>
+		<label>Password </label><input type="text" name="password"/>
+		<br>
 		<label>Name </label><input type="text" name="name"/>
 		<br>
 		<br>
 		<label>Address Line 1 </label><input type="text" name="address1"/> <!-- Street address, P.O. box, company name, c/o,   <div class="submissionfield">-->
 		<br>
-		<label>Address Line 2 </label><input type="text" name="address2"/> <!-- Apartment, suite, unit, building, floor, etc.,     <div class="submissionbutton"> -->
-		<br>
-		<div>
-			<label>Password </label>
-			<input type="text" name="password">
-		</div>
 		<label>City <label/><input type="text" name="city"/>
 		<br>
 		<label>State/Province/Region <label/><input type="text" name="region"/>	
