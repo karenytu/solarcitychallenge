cf472b960e548b483b3fdf60482c6454526c36e8
Karen Tu <karen.2.tu@gmail.com>
2017-03-08 03:47:25 -0800

    process broken

--- process.php
+++ process.php
@@ -1,3 +1,9 @@
+<html>
+		<body>
+		<label>x</label>
+	</body>
+</html>"
+
 <?php
 	$resource =  pg_connect(getenv('DATABASE_URL')); // postgres://zbvkfthmvsspzz:c57aa9f724109cc6ad419575424b765a10ee05205091f8048ad24c607545b3c8@ec2-23-21-186-138.compute-1.amazonaws.com:5432/d5arjtlq5lmgqh
 	$result = pg_query($resource, "IF OBJECT_ID('users') IS NULL");
@@ -11,12 +17,12 @@
 			require_once(usernameexists.html);
 		} else {
 			// landing page: you have been added to the mailing list to get any additional information! in the meantime, check out solarcity's website
-			//require_once(success.html);
-			echo "<html>
-				<body>
-					<label>Everything entered as normally</label>
-				</body>
-			</html>";
+			require_once(success.html);
+			// echo "<html>
+			// 	<body>
+			// 		<label>Everything entered as normally</label>
+			// 	</body>
+			// </html>";
 			
 			$serializedaddress = '';
 			// for the different fields of an address, concatenate into a single string
