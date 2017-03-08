dca19f6b7b747bc1a3e9eb55825cac2403fff542
Karen Tu <karen.2.tu@gmail.com>
2017-03-08 03:53:17 -0800

    process broken

--- process.php
+++ process.php
@@ -1,8 +1,8 @@
-<html>
+<!-- <html>
 		<body>
 		<label>x</label>
 	</body>
-</html>"
+</html> -->
 
 <?php
 	$resource =  pg_connect(getenv('DATABASE_URL')); // postgres://zbvkfthmvsspzz:c57aa9f724109cc6ad419575424b765a10ee05205091f8048ad24c607545b3c8@ec2-23-21-186-138.compute-1.amazonaws.com:5432/d5arjtlq5lmgqh
